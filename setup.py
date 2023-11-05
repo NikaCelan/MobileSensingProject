d47b6220ee29b0ffb77f1e69a85233a6261b140d
Hubert Michalski <michalsh@Huberts-MacBook-Air.local>
2023-11-05 18:05:19 +0100

    fix folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implement federated learning techniques for keyword recognition.',
+    author='Hubert Michalski, Nika Celan',
+    license='MIT',
+)
